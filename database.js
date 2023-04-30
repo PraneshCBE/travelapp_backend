62b6a6c9b6be3eaffe0984297981d7808477891c
pr <rvpspran@gmail.com>
2023-05-01 00:56:19 +0530

    initial

--- database.js
+++ database.js
@@ -3,12 +3,12 @@ require('dotenv').config()
 const dbconnection= mysql.createPool({
     host: process.env.HOST,
     user: process.env.USER,
-    password: process.env.PASSwORD,
+    password: process.env.PASSWORD,
     database: process.env.DATABASE
 }).promise()
 
 async function getUsers(){
-    const [rows]= await dbconnection.query("SELECT * FROM samp1")
+    const [rows]= await dbconnection.query("SELECT * FROM travel_app")
     return rows
 } 
 module.exports ={getUsers}
