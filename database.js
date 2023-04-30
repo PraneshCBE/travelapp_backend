8f07a0579631bcb7735202bb76d17cf3edb60b9d
pr <rvpspran@gmail.com>
2023-05-01 01:11:27 +0530

    c2

--- database.js
+++ database.js
@@ -1,6 +1,9 @@
 const mysql =require('mysql2')
 require('dotenv').config()
+console.log(process.env.HOST)
 console.log(process.env.USER)
+console.log(process.env.PASSWORD)
+console.log(process.env.DATABASE)
 const dbconnection= mysql.createPool({
     host: process.env.HOST,
     user: process.env.USER,
